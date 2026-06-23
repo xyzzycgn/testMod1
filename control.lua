7515718b47c3a25247abbe3874c9f0aff0a4d359
xyzzycgn <120264047+xyzzycgn@users.noreply.github.com>
2026-06-23 17:52:31 +0200

    Upgrade to V2.1 of base game

--- control.lua
+++ control.lua
@@ -1,33 +1,35 @@
 ---
 --- Created by xyzzycgn.
---- DateTime: 10.03.25 10:28
 ---
-local Log = require("__log4factorio__.Log")
+--local Log = require("__log4factorio__.Log")
 local sub = require("sub")
 
-Log.setSeverityFromSettings("testmod1-logLevel")
+
+
+--Log.setSeverityFromSettings("testmod1-logLevel")
 
 
 for ndx, wcid in pairs({ defines.wire_connector_id.circuit_red, defines.wire_connector_id.circuit_green,
                          defines.wire_connector_id.combinator_input_red, defines.wire_connector_id.combinator_input_green,
                          defines.wire_connector_id.combinator_output_red, defines.wire_connector_id.combinator_output_green }) do
-    Log.logBlock( { ndx = ndx, wcid = wcid }, function(m)log(m)end, Log.FINE)
+    --Log.logBlock( { ndx = ndx, wcid = wcid }, function(m)log(m)end, Log.FINE)
+    log(serpent.block({ ndx = ndx, wcid = wcid }))
 end
 
 
 
-Log.log("Test Mod 1 Log 1", function(m)log(m)end)
-Log.log("Test Mod 1 Log 2", function(m)log(m)end, Log.FINE)
-Log.log("Test Mod 1 Log 3", function(m)log(m)end, Log.WARN)
+--Log.log("Test Mod 1 Log 1", function(m)log(m)end)
+--Log.log("Test Mod 1 Log 2", function(m)log(m)end, Log.FINE)
+--Log.log("Test Mod 1 Log 3", function(m)log(m)end, Log.WARN)
 
 
 
 local function nth_tick_handler(e)
-    Log.log("Test Mod 1 Log nth tick", function(m)log(m)end, Log.FINE)
+    --Log.log("Test Mod 1 Log nth tick", function(m)log(m)end, Log.FINE)
 end
 
 local function nth_tick_handler2(e)
-    Log.log("Test Mod 1 Log nth tick2", function(m)log(m)end)
+    --Log.log("Test Mod 1 Log nth tick2", function(m)log(m)end)
 end
 
 
@@ -37,7 +39,7 @@ script.on_nth_tick(180, nth_tick_handler2)
 local function alterSetting(event, which, func)
     if event.setting == which then
         local new = settings.global[which].value
-        Log.log('setting ' .. which .. ' changed to ' .. new, function(m)log(m)end, Log.CONFIG)
+        --Log.log('setting ' .. which .. ' changed to ' .. new, function(m)log(m)end, Log.CONFIG)
         func(new)
         return true
     end
